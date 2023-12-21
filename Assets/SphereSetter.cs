5f12d53c2ff9a9fcd5321414ab8b6b1266fc68b2
Alexey Rival <isaev.alexey.rival@yandex.ru>
2023-12-21 20:05:50 +0300

    Init 2

--- Assets/SphereSetter.cs
+++ Assets/SphereSetter.cs
@@ -6,10 +6,9 @@ public class SphereSetter : MonoBehaviour
     private readonly string param = "_Shift";//избежать лишних аллокаций
     void Start()
     {
-        MaterialPropertyBlock block;   
+        MaterialPropertyBlock block = new MaterialPropertyBlock(); ;   
         for (int i = 0; i < spheres.Length; ++i)
         {
-            block = new MaterialPropertyBlock();
             block.SetVector(param, new Vector4(((i)%8)*.125f,1f-((i)/8*.25f)-.25f));
             spheres[i].SetPropertyBlock(block);
         }
